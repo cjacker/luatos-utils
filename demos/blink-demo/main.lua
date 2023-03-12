79657351c6fb4c0de108b02bd380fc30273352bf
Cjacker <cjacker@foxmail.com>
2023-03-12 17:38:40 +0800

    add ESP32S3 support to blink demo

--- demos/blink-demo/main.lua
+++ demos/blink-demo/main.lua
@@ -33,6 +33,8 @@ function pinx() -- 根据不同开发板，给LED赋值不同的gpio引脚编号
         return pin.PD14, pin.PD15, pin.PC3
     elseif rtos_bsp == "ESP32C3" then -- ESP32C3开发板的引脚
         return 12, 13, 255 -- 开发板上就2个灯
+    elseif rtos_bsp == "ESP32S3" then -- ESP32S3
+        return 12, 255, 255 -- LED连接到IO 12
     elseif rtos_bsp == "EC618" then -- Air780E开发板引脚
         return 27, 255, 255 -- AIR780E开发板上就一个灯
     else
