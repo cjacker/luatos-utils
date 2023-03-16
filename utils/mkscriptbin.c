96627486bcd3eafebfa8687f51db714f67c12e0c
Cjacker <cjacker@foxmail.com>
2023-03-16 15:32:57 +0800

    change tab to 4 space

--- utils/mkscriptbin.c
+++ utils/mkscriptbin.c
@@ -24,19 +24,19 @@ Node* add_node(Node *head, char *filename) {
 }
 
 void help() {
-	printf("luatos-mkscriptbin - a tool to generate script.bin for LuatOS\n");
-	printf("Usage : luatos-mkscriptbin <dist dir>\n");
+    printf("luatos-mkscriptbin - a tool to generate script.bin for LuatOS\n");
+    printf("Usage : luatos-mkscriptbin <dist dir>\n");
 }
 
 int main(int argc, char* argv[]) {
-	if(argc != 2) {
-		help();
-		exit(0);
-	}
+    if(argc != 2) {
+        help();
+        exit(0);
+    }
 
     DIR *dir;
     struct dirent *ent;
-	Node *head = NULL;
+    Node *head = NULL;
     char *disk_path = argv[1]; // disk path
     uint16_t file_count = 0;
 
@@ -49,7 +49,7 @@ int main(int argc, char* argv[]) {
     while ((ent = readdir(dir)) != NULL) {
         if (ent->d_type == DT_REG) { // if regular file
             //printf("%s\n", ent->d_name);
-			head = add_node(head, ent->d_name);
+            head = add_node(head, ent->d_name);
             file_count++;
         }
     }
@@ -57,10 +57,10 @@ int main(int argc, char* argv[]) {
     //printf("file count : %d\n", file_count);
     closedir(dir);
 
-	if(file_count == 0) {
-		fprintf(stderr, "Error : no file in %d dir", disk_path);
-		exit(1);
-	}
+    if(file_count == 0) {
+        fprintf(stderr, "Error : no file in %d dir", disk_path);
+        exit(1);
+    }
 
     FILE *fp;
     char filename[] = "script.bin";
@@ -70,96 +70,96 @@ int main(int argc, char* argv[]) {
         perror("open target file './script.bin' failed.");
         return 1;
     }
-	// magic number
+    // magic number
     uint8_t magic[] = {0x01, 0x04, 0x5A, 0xA5, 0X5A, 0xA5};
     int num_magic = sizeof(magic) / sizeof(uint8_t);
 
-	fwrite(magic, sizeof(uint8_t), num_magic, fp);
+    fwrite(magic, sizeof(uint8_t), num_magic, fp);
 
-	// version
-	uint8_t version[] = {0x02, 0x02, 0x02, 0x00};
-	//uint8_t version[] = {0x02, 0x02, 0x00, 0x02};
-	int num_version = sizeof(version) / sizeof(uint8_t);
-	fwrite(version, sizeof(uint8_t), num_version, fp);
+    // version
+    uint8_t version[] = {0x02, 0x02, 0x02, 0x00};
+    //uint8_t version[] = {0x02, 0x02, 0x00, 0x02};
+    int num_version = sizeof(version) / sizeof(uint8_t);
+    fwrite(version, sizeof(uint8_t), num_version, fp);
 
-	// head length, fixed length: 18, aka, 0x12
-	uint8_t headlen[] = {0x03, 0x04, 0x12, 0x00, 0x00, 0x00};
-	int num_headlen = sizeof(headlen) / sizeof(uint8_t);
-	fwrite(headlen, sizeof(uint8_t), num_headlen, fp);	
+    // head length, fixed length: 18, aka, 0x12
+    uint8_t headlen[] = {0x03, 0x04, 0x12, 0x00, 0x00, 0x00};
+    int num_headlen = sizeof(headlen) / sizeof(uint8_t);
+    fwrite(headlen, sizeof(uint8_t), num_headlen, fp);    
 
-	// file count, reverse order.
-	uint8_t filecount[4];
-	filecount[0] = 0x04;
-	filecount[1] = 0x02;
-	filecount[2] = (uint8_t)file_count;;
-	filecount[3] = (uint8_t)(file_count>>8);
-	int num_filecount = sizeof(filecount) / sizeof(uint8_t); 	
-	fwrite(filecount, sizeof(uint8_t), num_filecount, fp);
+    // file count, reverse order.
+    uint8_t filecount[4];
+    filecount[0] = 0x04;
+    filecount[1] = 0x02;
+    filecount[2] = (uint8_t)file_count;;
+    filecount[3] = (uint8_t)(file_count>>8);
+    int num_filecount = sizeof(filecount) / sizeof(uint8_t);     
+    fwrite(filecount, sizeof(uint8_t), num_filecount, fp);
 
-	// CRC, not used.
-	uint8_t crc[] = {0xFE, 0x02, 0xFF, 0xFF};
-	int num_crc = sizeof(crc) / sizeof(uint8_t);
-	fwrite(crc, sizeof(uint8_t), num_crc, fp);
+    // CRC, not used.
+    uint8_t crc[] = {0xFE, 0x02, 0xFF, 0xFF};
+    int num_crc = sizeof(crc) / sizeof(uint8_t);
+    fwrite(crc, sizeof(uint8_t), num_crc, fp);
 
- 	Node *current = head;
+    Node *current = head;
     while (current != NULL) {
-    	// magic number
-    	uint8_t magic[] = {0x01, 0x04, 0x5A, 0xA5, 0X5A, 0xA5};
-    	int num_magic = sizeof(magic) / sizeof(uint8_t);
+        // magic number
+        uint8_t magic[] = {0x01, 0x04, 0x5A, 0xA5, 0X5A, 0xA5};
+        int num_magic = sizeof(magic) / sizeof(uint8_t);
 
-    	fwrite(magic, sizeof(uint8_t), num_magic, fp);
+        fwrite(magic, sizeof(uint8_t), num_magic, fp);
 
-		// filename length
-		uint8_t filenamelen[] = {0x02, strlen(current->filename)};
-		int num_filenamelen = sizeof(filenamelen) / sizeof(uint8_t);
-    	fwrite(filenamelen, sizeof(uint8_t), num_filenamelen, fp);
-		
-		// filename
-		fwrite(current->filename, sizeof(uint8_t), strlen(current->filename), fp);
+        // filename length
+        uint8_t filenamelen[] = {0x02, strlen(current->filename)};
+        int num_filenamelen = sizeof(filenamelen) / sizeof(uint8_t);
+        fwrite(filenamelen, sizeof(uint8_t), num_filenamelen, fp);
+        
+        // filename
+        fwrite(current->filename, sizeof(uint8_t), strlen(current->filename), fp);
 
-		// open file to get file size and contents
+        // open file to get file size and contents
 
-		char *filename = malloc(strlen(disk_path) + strlen(current->filename) + 1);
-		sprintf(filename, "%s/%s", disk_path, current->filename);
-	
-    	FILE *file;
-    	long size;
-	
-    	file = fopen(filename, "rb"); 
-    	if (file == NULL) {
-        	fprintf(stderr, "open file failed : %s\n", filename);
-        	return 1;
-    	}
+        char *filename = malloc(strlen(disk_path) + strlen(current->filename) + 1);
+        sprintf(filename, "%s/%s", disk_path, current->filename);
+    
+        FILE *file;
+        long size;
+    
+        file = fopen(filename, "rb"); 
+        if (file == NULL) {
+            fprintf(stderr, "open file failed : %s\n", filename);
+            return 1;
+        }
 
-    	fseek(file, 0, SEEK_END); // seek to end
-    	size = ftell(file); 
+        fseek(file, 0, SEEK_END); // seek to end
+        size = ftell(file); 
 
-		// reset cursor	
-		fseek(file, 0, SEEK_SET);
-		char *content = malloc(size + 1);
-		fread(content, size, 1, file);
-		fclose(file);
-			
-		// file size, reverse order
-		uint8_t filesize[6];
-		filesize[0] = 0x03;
-		filesize[1] = 0x04;
-		filesize[2] = (uint8_t)(size);
-		filesize[3] = (uint8_t)(size >>8);
-		filesize[4] = (uint8_t)(size >>16);
-		filesize[5] = (uint8_t)(size >>24);
-		fwrite(filesize, sizeof(uint8_t), 6, fp);
-	
-		// crc, not used.
-		uint8_t crc[] = {0xFE, 0x02, 0xFF, 0xFF};
-		int num_crc = sizeof(crc) / sizeof(uint8_t);
-		fwrite(crc, sizeof(uint8_t), num_crc, fp);
+        // reset cursor    
+        fseek(file, 0, SEEK_SET);
+        char *content = malloc(size + 1);
+        fread(content, size, 1, file);
+        fclose(file);
+            
+        // file size, reverse order
+        uint8_t filesize[6];
+        filesize[0] = 0x03;
+        filesize[1] = 0x04;
+        filesize[2] = (uint8_t)(size);
+        filesize[3] = (uint8_t)(size >>8);
+        filesize[4] = (uint8_t)(size >>16);
+        filesize[5] = (uint8_t)(size >>24);
+        fwrite(filesize, sizeof(uint8_t), 6, fp);
+    
+        // crc, not used.
+        uint8_t crc[] = {0xFE, 0x02, 0xFF, 0xFF};
+        int num_crc = sizeof(crc) / sizeof(uint8_t);
+        fwrite(crc, sizeof(uint8_t), num_crc, fp);
 
-		// file contents
-		fwrite(content, sizeof(uint8_t), size, fp);
-		free(content);
+        // file contents
+        fwrite(content, sizeof(uint8_t), size, fp);
+        free(content);
 
-		// continue to next.	
+        // continue to next.    
         current = current->next;
     }
 
@@ -170,9 +170,9 @@ int main(int argc, char* argv[]) {
         current = current->next;
         free(temp->filename);
         free(temp);
-    }	
+    }    
 
     fclose(fp);
-	  //printf("script.bin generated.\n");
+    //printf("script.bin generated.\n");
     return 0;
 }
